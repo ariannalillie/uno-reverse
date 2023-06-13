9e43cb8908e3ba455c9cf728cb22e5afd2995c3f
Arianna Johnson <ariannajohnson588@gmail.com>
2023-06-13 15:28:57 -0400

    feat: add person interface and base person and computer class off of this

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,10 @@
 fun main() {
-    val player1 = Player("Player 1", mutableListOf())
-    val player2 = Player("Player 2", mutableListOf(), isComputer = true)
-    val players = listOf(player1, player2)
+    val players = listOf(
+        HumanPlayer(),
+        ComputerPlayer()
+    )
 
     val game = Game(players)
-    player1.game = game
-    player2.game = game
-
     game.setUpGame()
     game.playGame()
     game.checkForWin()
@@ -35,11 +33,7 @@ enum class CardColor(val textColor: String, val bgColor: String) {
     BLUE("\u001B[30m", "\u001B[44m"),
 }
 
-// Card class - card's color(red, green, blue, yellow) and value. wild, skip, reverse, draw 2
-class Card(
-    val color: CardColor,
-    val type: CardType
-) {
+class Card(val color: CardColor, val type: CardType) {
     override fun toString(): String {
         val reset = "\u001B[0m"
         val formattedText = "${color.textColor}${color.bgColor}${type.name}$reset"
@@ -47,164 +41,157 @@ class Card(
     }
 }
 
-// createDeck loops over the CardColor and CardType enum and creates a card of each type for each color
 fun createDeck(): List<Card> {
-    val deck = mutableListOf<Card>()
-
-    for (color in CardColor.values()) {
-        for (type in CardType.values()) {
-            deck.add(Card(color, type))
+    return CardColor.values().flatMap { color ->
+        CardType.values().map { type ->
+            Card(color, type)
         }
     }
-    return deck
 }
 
-// shuffleDeck returns the list of cards in a random order
 fun shuffleDeck(deck: List<Card>): MutableList<Card> {
     return deck.shuffled().toMutableList()
 }
 
-// Player class - player's name and hand (a list of cards).
-class Player(
-    val name: String,
-    private val hand: MutableList<Card>,
-    val isComputer: Boolean = false,
-    var game: Game? = null
-) {
-    fun showHand() {
+interface Player {
+    fun showHand()
+    fun showCurrentCard(index: Int): Card
+    fun drawCard(card: Card)
+    fun cardsLeft(): Int
+    fun playCard(currentCard: Card, deck: MutableList<Card>): Card?
+}
+
+class HumanPlayer : Player {
+    private val hand = mutableListOf<Card>()
+
+    override fun showHand() {
         println(hand)
     }
 
-    fun showCurrentCard(index: Int): Card {
+    override fun showCurrentCard(index: Int): Card {
         return hand[index]
     }
 
-    fun drawCard(card: Card) {
-        hand.add(card)
+    override fun drawCard(card: Card) {
+        hand += card
     }
 
-    fun cardsLeft(): Int {
+    override fun cardsLeft(): Int {
         return hand.size
     }
 
-    fun playCard(currentPlayer: Player, currentCard: Card, deck: MutableList<Card>) {
+    override fun playCard(currentCard: Card, deck: MutableList<Card>): Card? {
         var validCardPlayed = false
+        var playedCard: Card? = null
 
-        // user will be asked to continue to draw cards/ type a valid response until they play a valid card
         while (!validCardPlayed) {
-            println("Your cards are: ${currentPlayer.showHand()}")
+            println("Your cards are: $hand")
             print("Enter the index of the card you would like to play, or type -1 to draw: ")
-            val index: Int = readln().toInt()
+            val index: Int = readLine()?.toIntOrNull() ?: -2
+
             if (index == -1) {
                 val newCard = deck.removeAt(0)
                 drawCard(newCard)
             } else {
-                println("The card you choose is: ${currentPlayer.showCurrentCard(index)}")
-                val selectedCard = hand[index]
+                println("The card you choose is: ${showCurrentCard(index)}")
+                val selectedCard = showCurrentCard(index)
 
                 if (selectedCard.color == currentCard.color || selectedCard.type == currentCard.type) {
-                    hand.removeAt(index)
-                    game?.updateCurrentCard(selectedCard)
-                    game?.swapPlayer(selectedCard)
+                    hand.removeAt(index)  // Remove the card from the hand
+                    playedCard = selectedCard
                     validCardPlayed = true
                 } else {
                     println("This is not a valid move, please try again")
                 }
             }
         }
+        return playedCard
+    }
+}
+
+class ComputerPlayer : Player {
+    private val hand = mutableListOf<Card>()
+
+    override fun showHand() {
+        println(hand)
     }
 
-    fun computerMove(deck: MutableList<Card>) {
-        // EASY: finds three playable cards and randomly chooses one
-        val currentCardType = game?.currentCard?.type
-        val currentCardColor = game?.currentCard?.color
-        val playableCards = hand.filter { card -> card.type == currentCardType || card.color == currentCardColor }
+    override fun showCurrentCard(index: Int): Card {
+        return hand[index]
+    }
+
+    override fun drawCard(card: Card) {
+        hand += card
+    }
+
+    override fun cardsLeft(): Int {
+        return hand.size
+    }
+
+    override fun playCard(currentCard: Card, deck: MutableList<Card>): Card? {
+        val playableCards = hand.filter { card -> card.type == currentCard.type || card.color == currentCard.color }
 
         if (playableCards.isNotEmpty()) {
             val cardPicked = playableCards.random()
-            game?.updateCurrentCard(cardPicked)
-            val cardIndex = hand.indexOf(cardPicked)
-            hand.removeAt(cardIndex)
-            game?.swapPlayer(cardPicked)
+            hand.remove(cardPicked)  // Remove the card from the hand
             println("The computer played $cardPicked")
+            return cardPicked
         } else {
-            val newCard = deck.removeAt(0)
-            drawCard(newCard)
+            while (true) {
+                val newCard = deck.removeAt(0)
+                drawCard(newCard)
+                if (newCard.type == currentCard.type || newCard.color == currentCard.color) {
+                    println("The computer drew $newCard")
+                    return newCard
+                }
+            }
         }
     }
 }
 
-// Game class - players, the current player, and the current card on the table.
-class Game(
-    private val players: List<Player>
-) {
-    var currentPlayer = players[0]
-    var gameOver = false
-    var currentCard: Card? = null
-    // Used as lazy property delegate so that the deck will be created only when it is
-    // accessed for the first time
-    val deck: List<Card> by lazy { createDeck() }
-    // Used as lazy property delegate so that the shuffledDeck will be created only when
-    // it is accessed for the first time
-    val shuffledDeck: MutableList<Card> by lazy { shuffleDeck(deck) }
+class Game(private val players: List<Player>) {
+    private var currentPlayerIndex = 0
+    private var gameOver = false
+    private var currentCard: Card? = null
+    private val deck: List<Card> by lazy { createDeck() }
+    private val shuffledDeck: MutableList<Card> by lazy { shuffleDeck(deck) }
 
-    // startGame creates a deck, shuffles it and then deals 7 cards to each player
     fun setUpGame() {
-
         for (player in players) {
-            var count = 0
-            while (count < 7) {
+            repeat(7) {
                 player.drawCard(shuffledDeck.removeAt(0))
-                count++
             }
         }
         currentCard = shuffledDeck.removeAt(0)
     }
 
     fun playGame() {
-        players.forEach { player -> player.game = this }
-
         while (!gameOver) {
-//            if (!repeatedTurn) {
+            val currentPlayer = players[currentPlayerIndex]
             println(" ")
-            println("Current Player: ${currentPlayer.name}")
+            println("Current Player: ${currentPlayer.javaClass.simpleName}")
             println("The current card on the table is: ${currentCard.toString()}")
-//            } else {
-//                println("The computer drew a card")
-//            }
-            if (!currentPlayer.isComputer) {
-                currentPlayer.playCard(currentPlayer, currentCard!!, shuffledDeck)
-                // Computer loop logic
-            } else {
-                currentPlayer.computerMove(shuffledDeck)
+
+            val playedCard = currentPlayer.playCard(currentCard!!, shuffledDeck)
+            if (playedCard != null) {
+                currentCard = playedCard
             }
 
-
-            // Swap current player
+            currentPlayerIndex = (currentPlayerIndex + 1) % players.size
         }
     }
 
-    fun swapPlayer(selectedCard: Card) {
-        if (selectedCard.type == CardType.REVERSE || selectedCard.type == CardType.SKIP) {
-            currentPlayer = currentPlayer
-        } else {
-            currentPlayer = if (currentPlayer === players[0]) players[1] else players[0]
-        }
-    }
-
-    fun updateCurrentCard(playedCard: Card) {
-        currentCard = playedCard
-    }
-
-    // checkForWin checks to see if either the player or the computer has an empty hand to determine if
-    // anyone has won yet, and if so sets `gameOver` to true
     fun checkForWin() {
-        if (players[0].cardsLeft() == 0) {
-            gameOver = true
-            println("Congrats! You win 🎉")
-        } else if (players[1].cardsLeft() == 0) {
-            gameOver = true
-            println("The computer wins this round, better luck next time!")
+        players.forEachIndexed { index, player ->
+            if (player.cardsLeft() == 0) {
+                gameOver = true
+                if (index == 0) {
+                    println("Congrats! You win 🎉")
+                } else {
+                    println("The computer wins this round, better luck next time!")
+                }
+                return
+            }
         }
     }
 }
